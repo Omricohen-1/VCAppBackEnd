34c178c2c9a416eb05f4ca41db9656f6704511c0
Nitzan Hadar <nitzanehadar@gmail.com>
2020-09-01 20:29:54 +0300

    initial

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS, cross_origin
+from api import AvailableUniversities, AcceptanceResults, HighSchoolScore, AvailableHighSchoolSubjects
+
+application = Flask(__name__)
+cors = CORS(application)
+application.config['CORS_HEADERS'] = 'Content-Type'
+
+
+api = Api(application)  
+
+
+
+@application.route("/status")
+def status():
+    return "ok"
+
+
+if __name__ == "__main__":
+    application.run(port=8080, debug=True)
