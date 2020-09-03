e2835cb09c0ee12fa7b7aa519bc673139f6b5af6
Nitzan Hadar <nitzanehadar@gmail.com>
2020-09-03 18:51:03 +0300

    [linkedin] linkedin search class and func

--- social/linkedin/linkedin.py
+++ social/linkedin/linkedin.py
@@ -1,36 +1,91 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from bs4 import BeautifulSoup as bs
 import json
 
-driver = webdriver.Chrome(r'.\chromedriver')
-driver.get('https://www.linkedin.com/')
+
+# TODO remove test data and get parmaters
 test_data = json.loads(open('mocks\FB_TEST.json', 'r').read())
 
 
-#TODO add a check to see if connected or not and the manage connection
-def sign_in():
-    usename_place = driver.find_element_by_xpath('//*[@id="session_key"]')
-    usename_place.click()
-    usename_place.send_keys(test_data['email'])
+class LinkedinInstance:
+    def __init__(self):
+        self.main_url = 'https://www.linkedin.com'
+        self.driver = webdriver.Chrome(r'.\chromedriver')
+        self.driver.get(self.main_url)
 
-    usename_place = driver.find_element_by_xpath('//*[@id="session_password"]')
-    usename_place.click()
-    usename_place.send_keys(test_data['password'])
+    # TODO add a check to see if connected or not and the manage connection
+    def sign_in(self):
+        usename_place = self.driver.find_element_by_xpath(
+            '//*[@id="session_key"]')
+        usename_place.click()
+        usename_place.send_keys(test_data['email'])
 
-    driver.find_element_by_xpath(
-        '/html/body/main/section[1]/div[2]/form/button').click()
+        usename_place = self.driver.find_element_by_xpath(
+            '//*[@id="session_password"]')
+        usename_place.click()
+        usename_place.send_keys(test_data['password'])
 
-    try:
-        confirmition = driver.find_element_by_xpath(
-            '//*[@id="ember512"]/button[1]')
-        confirmition.click()
+        self.driver.find_element_by_xpath(
+            '/html/body/main/section[1]/div[2]/form/button').click()
 
-    except:
-        None
+        try:
+            confirmition = self.driver.find_element_by_xpath(
+                '//*[@id="ember512"]/button[1]')
+            confirmition.click()
 
-sign_in()
+        except:
+            None
 
-search_bar = driver.find_element_by_xpath(
-    '//*[@id="ember39"]/div[2]/button').click()
-search_bar.send_keys('Aviv Sharon')
-search_bar.click()
+        # TODO arrange until
+        WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="ember41"]/input')))
+
+    def search(self, search_string):
+        search_bar = self.driver.find_element_by_xpath(
+            '//*[@id="ember41"]/input')
+        search_bar.send_keys(search_string)
+        search_bar.send_keys(Keys.ENTER)
+        WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'search-result__wrapper')))
+
+    def parse_search_results(self):
+        html_source = self.driver.page_source
+        soup = bs(html_source, "html")
+        search_object_list = []
+        for el in soup.find_all("div", class_="search-result__wrapper"):
+            search_object_list.append(self.parse_single_elment(el))
+
+        return search_object_list
+
+    def parse_single_elment(self, banner_element):
+        # TODO manage a state and log if more then 1 finding
+        # TODO add shared connections url search and parse
+        # TODO add shared connections count
+        result = {}
+        result['name'] = [element.text for element in banner_element.find_all(
+            "span", class_="name actor-name")][0]
+        result['profile_url'] = self.main_url+[element["href"]
+                                               for element in banner_element.find_all("a", class_="search-result__result-link ember-view")][0]
+        result['role'] = [element.text for element in banner_element.find_all(
+            "p", class_="subline-level-1 t-14 t-black t-normal search-result__truncate")][0]
+        result['location'] = [element.text for element in banner_element.find_all(
+            "p", class_="subline-level-2 t-12 t-black--light t-normal search-result__truncate")][0]
+
+        return result
+
+
+def get_users_by_search(search_string):
+    # TODO Make it headless
+    # TODO add headers to vonnevt as his default browser?
+    linkedin_instance = LinkedinInstance()
+    linkedin_instance.sign_in()
+    linkedin_instance.search(search_string)
+    linkedin_instance.parse_search_results()
+
+
+test = get_users_by_search('Aviv Sharon')
