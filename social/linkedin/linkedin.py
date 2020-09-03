18872d6013c91bce653d62a99071fe81a8660381
Nitzan Hadar <nitzanehadar@gmail.com>
2020-09-03 17:29:47 +0300

    [linkedin] sapareted signin func

--- social/linkedin/linkedin.py
+++ social/linkedin/linkedin.py
@@ -3,31 +3,32 @@ from bs4 import BeautifulSoup
 import json
 
 driver = webdriver.Chrome(r'.\chromedriver')
-
 driver.get('https://www.linkedin.com/')
-
 test_data = json.loads(open('mocks\FB_TEST.json', 'r').read())
 
 
-usename_place = driver.find_element_by_xpath('//*[@id="session_key"]')
-usename_place.click()
-usename_place.send_keys(test_data['email'])
+#TODO add a check to see if connected or not and the manage connection
+def sign_in():
+    usename_place = driver.find_element_by_xpath('//*[@id="session_key"]')
+    usename_place.click()
+    usename_place.send_keys(test_data['email'])
 
+    usename_place = driver.find_element_by_xpath('//*[@id="session_password"]')
+    usename_place.click()
+    usename_place.send_keys(test_data['password'])
 
-usename_place = driver.find_element_by_xpath('//*[@id="session_password"]')
-usename_place.click()
-usename_place.send_keys(test_data['password'])
+    driver.find_element_by_xpath(
+        '/html/body/main/section[1]/div[2]/form/button').click()
 
-driver.find_element_by_xpath(
-    '/html/body/main/section[1]/div[2]/form/button').click()
+    try:
+        confirmition = driver.find_element_by_xpath(
+            '//*[@id="ember512"]/button[1]')
+        confirmition.click()
 
-try:
-    confirmition = driver.find_element_by_xpath(
-        '//*[@id="ember512"]/button[1]')
-    confirmition.click()
+    except:
+        None
 
-except:
-    None
+sign_in()
 
 search_bar = driver.find_element_by_xpath(
     '//*[@id="ember39"]/div[2]/button').click()
