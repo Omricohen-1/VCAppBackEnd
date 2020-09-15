6f1dc611ca4a5ffd78fb9f4345ba29fd9cc1fe5d
Omri Cohen <omricoh1@gmail.ocm>
2020-09-16 00:28:49 +0300

    added another option to chrome options

--- social/linkedin/linkedin_instance.py
+++ social/linkedin/linkedin_instance.py
@@ -14,6 +14,7 @@ class LinkedinInstance:
     def __init__(self, email, password):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--allow-running-insecure-content')
         chrome_options.add_argument("--window-size=1920x1080")
         self.main_url = 'https://www.linkedin.com'
         self.driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=chrome_options)
