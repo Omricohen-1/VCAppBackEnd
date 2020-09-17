efce17051ba4634d2e85ef2be2a9e21146db1b48
Omri Cohen <omricoh1@gmail.ocm>
2020-09-17 17:05:13 +0300

    added option disable gpu

--- social/linkedin/linkedin_instance.py
+++ social/linkedin/linkedin_instance.py
@@ -16,7 +16,9 @@ import json
 class LinkedinInstance:
     def __init__(self, email, password):
         options = Options()
-        options.headless = True
+        options.add_argument('--headless')
+        options.add_argument('--hide-scrollbars')
+        options.add_argument('--disable-gpu')
         # display = Display(visible=0, size=(1024, 768))
         # display.start()
         # chrome_options = webdriver.ChromeOptions()
@@ -30,7 +32,7 @@ class LinkedinInstance:
         self.main_url = 'https://www.linkedin.com'
         # self.driver = webdriver.Chrome("/home/ubuntu/chrome_driver/chromedriver", chrome_options=chrome_options)
         self.driver.get(self.main_url)
-        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="session_key"]')))
+        element = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="session_key"]')))
         if element:
             print('[LinkedinInstance] Sign-in requierd')
             self.sign_in(email, password)
