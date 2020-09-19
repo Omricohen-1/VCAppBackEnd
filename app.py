ecc15480a46ec931c8f968cfa3d8ba86571bf717
Nitzan Hadar <nitzanehadar@gmail.com>
2020-09-20 01:09:01 +0300

    bug fix in auth

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Api
 from flask_cors import CORS, cross_origin
 from add_contact_api import add_contact
-from linkedin import li_auth  ,li_search ,li_launch_auth
+from linkedin import li_auth, li_search, li_launch_auth
 
 from dynaconf import settings
 
@@ -14,15 +14,15 @@ application.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(application)
 
 api.add_resource(add_contact, "/add-contacts")
-api.add_resource(li_auth,'/linkedin_auth')
-api.add_resource(li_search,'/search_linkedin')
-api.add_resource(li_launch_auth,'/launch_linkedin_auth')
-
+api.add_resource(li_auth, '/linkedin_auth')
+api.add_resource(li_search, '/search_linkedin')
+api.add_resource(li_launch_auth, '/launch_linkedin_auth')
 
 
 @application.route("/status")
 def status():
     return "ok"
+
 
 if __name__ == "__main__":
     application.run(port=8080, debug=True)
--- linkedin.py
+++ linkedin.py
@@ -1,8 +1,13 @@
 from flask_restful import Resource
 from flask import request
 from social.linkedin_oauth2 import get_access_token
+from social.linkedin_oauth2.auth import main as auth
 from social.linkedin import linkedin_instance as li
 
+class li_launch_auth(Resource):
+    def get(self):
+        auth()
+        
 class li_auth(Resource):
     def get(self):
         if 'code' in request.args:
@@ -15,26 +20,17 @@ class li_auth(Resource):
            
 
 class li_search(Resource):
-    def search_linkedin():
-    """
-    Function excpects GET request with:
-    q='{str}' arg
-    email='{str}'
-    password='{str}'
-    """
-    # TODO change to oauth2
-    search_string = request.args.get('q')
-    email = request.args.get('email')
-    password = request.args.get('password')
-    return li.LinkedinInstance(email, password).get_users_by_search(search_string)
+    def search_linkedin(self):
+        """
+        Function excpects GET request with:
+        q='{str}' arg
+        email='{str}'
+        password='{str}'
+        """
+        # TODO change to oauth2
+        search_string = request.args.get('q')
+        email = request.args.get('email')
+        password = request.args.get('password')
+        return li.LinkedinInstance(email, password).get_users_by_search(search_string)
 
 
-class li_launch_auth(Resource):
-    def get(self):
-        if 'code' in request.args:
-            code = request.args.get('code')
-            state = request.args.get('state')
-            get_access_token.main(code)
-            return "got code processing... redirect in less then 5 sec"
-        else :
-            return "Ok"
