bdec807238e86a82cda7786c4811dd67a7dfd8f7
yaelco94 <yaelco94@gmail.com>
2020-09-20 00:43:34 +0300

    separation to functions: option to threshold by date and user

--- integration_with_google.py
+++ integration_with_google.py
@@ -1,64 +1,73 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gmail_quickstart]
 from __future__ import print_function
 import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import datetime
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+THRESHOLD = 21  # days
+user = "noreply@hebits.net"
 
 
-def main():
+def authentication():
     """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
+        Lists the user's Gmail labels.
+        """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token1:
-            creds = pickle.load(token1)
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(r'C:\Users\yaelc\Desktop\\vcApp\Backend\credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=81)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token1:
-            pickle.dump(creds, token1)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
     service = build('gmail', 'v1', credentials=creds)
+    return service
 
-    # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
 
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+def create_threshold_date(threshold):
+    today_date = datetime.datetime.today().date()
+    threshold_diff = datetime.timedelta(days=threshold)
+    threshold_date = today_date - threshold_diff
+    return f"{threshold_date.year}/{threshold_date.month}/{threshold_date.day}"
+
+
+def bring_all_mess(service):
+    threshold_date = create_threshold_date(1)
+    allMes = service.users().messages().list(userId='me', q=f"after:{threshold_date}").execute()
+    for mess in allMes['messages']:
+        messId = mess['id']
+        message = service.users().messages().get(userId='me', id=messId).execute()
+        print(message)
+
+
+def bring_filtered_mess(service, user, threshold):
+    threshold_date = create_threshold_date(threshold)
+    allMes = service.users().messages().list(userId='me', q=f"from:{user}, after:{threshold_date}").execute()
+    for mess in allMes['messages']:
+        messId = mess['id']
+        message = service.users().messages().get(userId='me', id=messId).execute()
+        print(message)
+
+
+def main():
+    service = authentication()
+    bring_all_mess(service)
 
 
 if __name__ == '__main__':
     main()
-# [END gmail_quickstart]
