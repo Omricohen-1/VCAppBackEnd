c17e2c41d54a7a6a9ec3a4ed72004e15f3c23eef
ShadowForker <42767816+ShadowForker@users.noreply.github.com>
2020-09-21 21:41:34 +0300

    fix get in linkedin search

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 from add_contact_api import add_contact
 from social.linkedin import linkedin_instance as li
 from social.facebook import facebook
-from elasticsearch import Elasticsearch
+# from elasticsearch import Elasticsearch
 from linkedin import li_auth, li_search, li_launch_auth
 from dynaconf import settings
 
@@ -16,8 +16,8 @@ application.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(application)
 
 api.add_resource(add_contact, "/add-contacts")
-elastic = Elasticsearch(['https://search-omri-elflj5ij34pitprcoersng2dfm.eu-central-1.es.amazonaws.com:443'],
-                        http_auth=('vcapp_test', 'CliqueHub1!'))
+# elastic = Elasticsearch(['https://search-omri-elflj5ij34pitprcoersng2dfm.eu-central-1.es.amazonaws.com:443'],
+#                         http_auth=('vcapp_test', 'CliqueHub1!'))
 
 #linkedin paths
 api.add_resource(li_auth, '/linkedin/auth')
